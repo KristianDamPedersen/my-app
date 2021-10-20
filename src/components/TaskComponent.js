637ad2cfe0245518263f7c9fbb343e9d28b7cdcf
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-10-20 17:19:27 +0200

    Added x'es to tasks

--- src/components/TaskComponent.js
+++ src/components/TaskComponent.js
@@ -1,10 +1,10 @@
 // import the x - icon
-import { FaAppStoreya } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
 
 const TaskComponent = (props) => {
     return (
         <div className='task'>
-            <h3>{props.task.text}</h3>
+            <h3>{props.task.text} <FaTimes /></h3>
             <p>{props.task.day}</p>
             
         </div>
