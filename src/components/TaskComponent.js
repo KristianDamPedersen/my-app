e4d12a18f9c8d86fe46c184c3d5dff244b6cf6a3
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-10-20 17:22:50 +0200

    Made the x'es red

--- src/components/TaskComponent.js
+++ src/components/TaskComponent.js
@@ -4,8 +4,12 @@ import { FaTimes } from 'react-icons/fa'
 const TaskComponent = (props) => {
     return (
         <div className='task'>
-            <h3>{props.task.text} <FaTimes /></h3>
-            <p>{props.task.day}</p>
+            <h3>
+                {props.task.text} <FaTimes style={{ color: 'red', cursor: 'pointer'}}/>
+            </h3>
+            <p>
+                {props.task.day}
+            </p>
             
         </div>
     )
