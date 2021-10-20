95737bb1b7f5798c62f1b59ef668d8723f8c3a3c
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-10-20 15:58:54 +0200

    Added task element

---
+++ src/Tasks.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+const Tasks = () => {
+    return (
+        <div>
+            <h1>This is the task stuff</h1>
+        </div>
+    )
+}
+
+
+// Define defaultProps
+Tasks.defaultProps = {
+
+}
+
+// Define propTypes
+Tasks.propTypes = {
+
+}
+
+
+export default Tasks
